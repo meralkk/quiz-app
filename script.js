7f2c173080c91dc1e6ad2b2878678393cf8100f2
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-02-18 09:21:27 +0100

    questions added

---
+++ script.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: true},
+            {text: "Girafee", correct: true}
+        ]
+    },
+    {
+        question: "Which is smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Sri Lanka", correct: false}
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true}
+        ]
+    },
+    {
+        question: "Which is smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false}
+        ]
+    }
+
+
+]
